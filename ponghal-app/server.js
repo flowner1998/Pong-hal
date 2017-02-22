82363f920284f9f7a7c99564f3bf260fa2d2e758
FLORIS-PC <flowner@hotmail.com>
2017-02-22 14:15:52 +0100

    -It actually works now :)

--- ponghal-app/server.js
+++ ponghal-app/server.js
@@ -5,7 +5,8 @@ var io = require('socket.io')(server);
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
+
 
 app.get('/', function(req, res){
 	res.sendfile('index.html');
@@ -14,16 +15,32 @@ app.get('/', function(req, res){
 app.get('/player-1', function(req, res){
     res.sendfile('player-1.html');
 });
+app.get('/player-2', function(req, res){
+    res.sendfile('player-2.html');
+});
 
 io.on('connection', function(socket){
   socket.on('disconnect', function(){
 
   });
 
-    socket.on('player 1 movement', function (data) {
-        console.log('player 1 movement: ' + data);
-        io.emit('player 1 movement', data);
+    socket.on('player1 up', function (data) {
+        console.log('player1 up: ' + data);
+        io.emit('player1 up', data);
     });
+    socket.on('player1 down', function (data) {
+        console.log('player1 down: ' + data);
+        io.emit('player1 down', data);
+    });
+    socket.on('player2 up', function (data) {
+        console.log('player2 up: ' + data);
+        io.emit('player2 up', data);
+    });
+    socket.on('player2 down', function (data) {
+        console.log('player2 down: ' + data);
+        io.emit('player2 down', data);
+    });
+
 });
 
 server.listen(300, function(){
